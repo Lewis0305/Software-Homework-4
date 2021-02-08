b77718709ce2fe689d918698679a55cca99c8059
Lewis0305 <46588124+Lewis0305@users.noreply.github.com>
2021-02-07 22:23:26 -0800

    Names program

---
+++ names.py
@@ -0,0 +1,11 @@
+def name(first, last):
+
+    try:
+        vfirst = str(first)
+        vlast = str(last)
+        if (min(len(vfirst), len(vlast)) > 0):
+            return vfirst + " " + vlast
+        else:
+            return "";
+    except ValueError:
+        return ""
